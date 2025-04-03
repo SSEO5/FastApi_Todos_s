88fb72f8853cda21f166cc28bba0483fccd81519
SSEO5 <wjdtjdud530@naver.com>
2025-04-03 20:04:57 +0900

    Add CORSMiddleware & .env setting

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -5,10 +5,19 @@ from pydantic import BaseModel
 import json
 import os
 import datetime
+from fastapi.middleware.cors import CORSMiddleware
 from enum import Enum
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class TodoStatus(str, Enum):
